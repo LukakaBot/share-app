cecfc02a902755034d6923ed417c8c13a477c6b7
Lukaka <monishine_code@163.com>
2024-11-05 15:14:47 +0800

    chore: 修改 main.js 里的 unocss 导入文件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createSSRApp } from 'vue';
 import App from './App.vue';
-// import 'uno.css';
-import 'virtual:uno.css'
+import 'uno.css';
 import router from '@/router';
 
 export function createApp() {
